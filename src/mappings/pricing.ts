944a5593da1a317e530fe6b032369ab8e830896c
Nichita Cravcenco <nice_arti@mail.ru>
2021-12-06 20:43:31 +0200

    factory

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -51,15 +51,17 @@ let WHITELIST: string[] = [
   '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
   '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
   '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
+  '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
+  '0x960b236a07cf122663c4303350609a66a7b288c0', // ANT
+  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
+  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
   '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
   '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
   '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
   '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
   '0x06b32E7Eb6F818c859e69334AC75f9c3b9AecF62', // IXS
+  '0xB1519Ffe2761Eb68C11F53eBb550f71C4E04C35F', // ISXgov
+//'0xLAkfjaasdflk3fj38raasdfkj49d84hjlfjakl34', // Fantom stable
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
