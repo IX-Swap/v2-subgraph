571d1ee8ba8b1ac891f59f84f29b8caa6d514ec2
Mihail Ceban <mihail.ceban@titanium-soft.com>
2022-03-30 16:19:10 +0300

    subgraph-deployed

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -259,13 +259,6 @@ export function handleSync(event: Sync): void {
   
   pair.trackedReserveETH = trackedLiquidityETH
 
-  //hardcoded way :D
-  const _iusdcPair = factoryContract.getPair
-                (Address.fromString('0xd0a1e359811322d97991e03f863a0c30c2cf029c'), // weth
-                Address.fromString('0xbc55ad5733a1bb050f51bbdfb65ecc7a72aedc20'))  // iusdc
-
-  let iusdcPair = Pair.load(_iusdcPair.toHex()) 
-
   pair.reserveETH = 
     pair.reserve0
     .times(token0.derivedETH as BigDecimal)
